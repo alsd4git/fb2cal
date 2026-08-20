41b4078a94da79e9cf4b6b4f21ed3e1f66af0284
Alessandro Digilio <alessandro.digilio@hotmail.it>
2026-08-20 19:28:00 +0200

    build: 📦 make uv the only supported packaging workflow
    Remove the legacy Pipenv and setuptools metadata, keep dependencies in pyproject.toml, and refresh the lockfile for the fork. Update distribution metadata to identify the maintained fork and its upstream project.

--- fb2cal/__meta__.py
+++ fb2cal/__meta__.py
@@ -1,20 +1,39 @@
-__author__ = 'Mo Beigi'
-__copyright__ = 'Copyright 2019-2020'
-__description__ = 'Facebook Birthday Events to ICS file converter'
-__email__ = 'me@mobeigi.com'
-__keywords__ = ['facebook', 'birthday', 'calendar', 'export', 'ics']
+__author__ = "Alessandro Digilio"
+__copyright__ = "Copyright 2019-2020 Mo Beigi; fork maintained by Alessandro Digilio"
+__description__ = "Fetch Facebook birthdays and export calendar/contact files"
+__email__ = ""
+__keywords__ = ["facebook", "birthday", "calendar", "export", "ics"]
 __license__ = "GPLv3"
-__maintainer__ = 'Mo Beigi'
-__status__ = 'Production'
-__title__ = 'fb2cal'
-__version_info__ = (1, 3, 2)
-__version__ = '.'.join(map(str, __version_info__))
+__maintainer__ = "Alessandro Digilio"
+__status__ = "Production"
+__title__ = "fb2cal"
+__version_info__ = (2, 0, 0)
+__version__ = ".".join(map(str, __version_info__))
 
-__github_url__ = 'https://github.com/mobeigi/fb2cal'
-__github_short_url__ = 'https://git.io/fjMwr'
-__github_assets_absolute_url__ = 'https://raw.githubusercontent.com/mobeigi/fb2cal/main'
-__download_url__= f'https://github.com/mobeigi/fb2cal/archive/v{__version__}.tar.gz'
+__github_url__ = "https://github.com/alsd4git/fb2cal"
+__github_short_url__ = __github_url__
+__github_assets_absolute_url__ = (
+    "https://raw.githubusercontent.com/alsd4git/fb2cal/main"
+)
+__download_url__ = f"https://github.com/alsd4git/fb2cal/archive/v{__version__}.tar.gz"
+__upstream_url__ = "https://github.com/mobeigi/fb2cal"
 
 
 # Make metadata public to script
-__all__ = ['__author__', '__copyright__', '__description__', '__download_url__', '__email__', '__github_assets_absolute_url__', '__github_short_url__', '__github_url__', '__license__', '__maintainer__', '__status__', '__title__', '__version__', '__version_info__']
+__all__ = [
+    "__author__",
+    "__copyright__",
+    "__description__",
+    "__download_url__",
+    "__email__",
+    "__github_assets_absolute_url__",
+    "__github_short_url__",
+    "__github_url__",
+    "__license__",
+    "__maintainer__",
+    "__status__",
+    "__title__",
+    "__upstream_url__",
+    "__version__",
+    "__version_info__",
+]
